0fb5e9439f1cbd1166bff7de466470d161c1df00
Javier <jhcastrolil@gmail.com>
2021-05-17 00:46:56 +0100

    create modules page.module and shared.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,33 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+
 import { LoginComponent } from './login/login.component';
-import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { Graficas1Component } from './pages/graficas1/graficas1.component';
-import { HeaderComponent } from './shared/header/headercomponent';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
-import { ProgressComponent } from './pages/progress/progress.component';
+import { RegisterComponent } from './login/register.component';
+
+//Rutas
 import { APP_ROUTES } from './app.routes';
-import { PagesComponent } from './pages/pages.component';
+
+//Modulos
+import { PagesModule } from './pages/pages.module';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    NopagefoundComponent,
-    DashboardComponent,
-    Graficas1Component,
-    HeaderComponent,
-    SidebarComponent,
-    BreadcrumbsComponent,
-    ProgressComponent,
-    PagesComponent
+    LoginComponent,     
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
+    PagesModule,    
     APP_ROUTES
   ],
   providers: [],
---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,30 @@
+import { PAGES_ROUTES } from './pages.routes';
+import { NgModule } from "@angular/core";
+
+import { PagesComponent } from './pages.component';
+
+import { ProgressComponent } from './progress/progress.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { Graficas1Component } from './graficas1/graficas1.component';
+import { SharedModule } from '../shared/shared.module';
+
+
+@NgModule({
+    declarations: [
+        DashboardComponent,
+        ProgressComponent,
+        Graficas1Component,
+        PagesComponent,
+    ],
+    exports:[
+        DashboardComponent,
+        ProgressComponent,
+        Graficas1Component,
+    ],
+    imports:[
+        SharedModule,
+        PAGES_ROUTES
+    ]
+})
+
+export class PagesModule{}

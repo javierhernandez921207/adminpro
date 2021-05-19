23f6c13fc06b3cdfafae1a28ffca9d21d8bf9a6d
Javier <javier.hernandez@ntsprint.com>
2021-05-19 16:03:06 -0400

    Fin de la seccion 7

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -1,16 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+declare function init_plugins();
+
 @Component({
   selector: 'app-pages',
   templateUrl: './pages.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class PagesComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    init_plugins();
   }
 
+  ngOnInit(): void {}
 }
